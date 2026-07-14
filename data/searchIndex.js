696383d455aa4e452d983c0935e1cb721e4e6148
Paulchen-coder <Paul_Bittner@gmx.de>
2026-07-14 12:21:56 +0200

    Add: Zentrale Suchindex-Datei mit allen Seiten

---
+++ data/searchIndex.js
@@ -0,0 +1,18 @@
+// Zentrale Suchindex-Datei mit ALLEN Seiten
+const searchIndex = [
+    {
+        title: "Startseite",
+        url: "/index.html",
+        keywords: ["startseite", "homepage", "mathematik", "elternportal", "klasse 7", "klasse 8", "prozentrechnung", "bruchrechnung", "lernen", "hilfe", "unterstützung", "homepage", "startseiten", "mathematik", "elternportale", "klasse", "klassen", "prozentrechnung", "bruchrechnungen", "lernt", "hilfen", "unterstützungen"]
+    },
+    {
+        title: "Klasse 7 – Übersicht",
+        url: "/klasse7/index.html",
+        keywords: ["klasse 7", "übersicht", "themen", "mathematik", "prozentrechnung", "bruchrechnung", "terme", "variablen", "gleichungen", "proportionalität", "geometrie", "klasse", "klassen", "übersichten", "thema", "mathematik", "prozent", "bruch", "term", "variable", "gleichung", "proportional", "geometrisch"]
+    },
+    {
+        title: "Klasse 8 – Übersicht",
+        url: "/klasse8/index.html",
+        keywords: ["klasse 8", "übersicht", "themen", "mathematik", "funktionen", "lineare funktionen", "quadratische funktionen", "potenzen", "potenzgesetze", "wahrscheinlichkeit", "pythagoras", "körper", "volumen", "klasse", "klassen", "übersichten", "thema", "mathematik", "funktion", "linear", "quadratisch", "potenz", "wahrscheinlich", "körpern"]
+    }
+];
